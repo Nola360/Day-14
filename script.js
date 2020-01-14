a9900f8b7e41e5efc820ebe838edec050ea4b4b6
nola360 <adaramolajr@gmail.com>
2020-01-14 13:56:23 -0500

    calculate amount interest and monthly payments added error and clear error functions

---
+++ script.js
@@ -0,0 +1,69 @@
+// Listen for submit
+document.querySelector('#loan-form').addEventListener('submit', calculateResults);
+
+// Calculate Results
+function calculateResults(e) {
+  // console.log('calculating...');
+
+  // Grab UI Vars
+  const amount = document.querySelector('#amount');
+  const interest = document.querySelector('#interest');
+  const years = document.querySelector('#years');
+  const monthlyPayment = document.querySelector('#monthly-payment');
+  const totalPayment = document.querySelector('#total-payment');
+  const totalInterest = document.querySelector('#total-interest');
+
+  // Amount is value but will turn to decimal with parseFloat()
+  const principle = parseFloat(amount.value);
+
+  // Calculated interest
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+
+  // Calculated Payments
+  const calculatedPayments = parseFloat(years.value) * 12;
+
+  // Compute monthly payment
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principle * x * calculatedInterest) / (x - 1);
+
+  // Check to see if method is finite #
+
+  if (isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = ((monthly * calculatedPayments) - principle).toFixed(2);
+  } else {
+    showError('Numbers are incorrect, Please check numbers')
+    console.log('please check numbers...');
+
+  }
+
+  e.preventDefault();
+}
+
+// Show Error
+function showError(error) {
+  // Create div
+  const errorDiv = document.createElement('div');
+
+  // Get Element (where message will display in html)
+  const card = document.querySelector('.card')
+  const heading = document.querySelector('.heading');
+
+  // Add class (string is BS code for error)
+  errorDiv.className = 'alert alert-danger';
+
+  // Create text node and append to div
+  errorDiv.appendChild(document.createTextNode(error));
+
+  // Insert Error above heading
+  card.insertBefore(errorDiv, heading);
+
+  // Clear error after 3 seconds
+  setTimeout(clearError, 3000)
+}
+
+// Clear Error
+function clearError() {
+  document.querySelector('.alert').remove();
+}
